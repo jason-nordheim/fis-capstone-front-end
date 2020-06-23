22ab06c508baa77ab9d211d966d45be3dcbdcf3a
jason-nordheim <jason.nordheim@gmail.com>
2020-06-23 11:28:07 -0600

    shortened methods

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,4 @@
-export const saveToken = (token) => {
-    console.log('saving token')
-    localStorage.setItem('token', token.token)
-    console.log('token saved')
-}
+export const saveToken = (token) => localStorage.setItem('token', token.token)
+export const getToken = () => localStorage.getItem('token')
+export const capatalize = (string) => string.toString().charAt(0).toUpperCase() + string.toString().slice(1);
 
-export const getToken = () => {
-    return localStorage.getItem('token')
-}
-
-
-export const capatalize = (string) => {
-    return string.toString().charAt(0).toUpperCase() + string.toString().slice(1);
-}
